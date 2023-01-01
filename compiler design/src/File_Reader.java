35714ddf3f09af82eb1070d1e5c0434232bfd73f
george <jaber.george2@gmail.com>
2023-01-01 21:22:24 +0200

    .

--- compiler design/src/File_Reader.java
+++ compiler design/src/File_Reader.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class File_Reader{
-    FileReader file_reader = new FileReader("compiler design//src//code.txt");
+    FileReader file_reader = new FileReader("compiler design/code.txt");
     BufferedReader br = new BufferedReader(file_reader);
 
     public File_Reader() throws IOException {
