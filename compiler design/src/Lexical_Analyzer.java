d965de82ab35a8e056721de0a2677ef7a6ea10e4
moashqar444 <m.ashqar51@gmail.com>
2023-01-02 12:19:03 +0200

    noting

--- compiler design/src/Lexical_Analyzer.java
+++ compiler design/src/Lexical_Analyzer.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class Lexical_Analyzer {
     FileReader file_reader = new FileReader("code.txt");
     BufferedReader br = new BufferedReader(file_reader);
@@ -9,7 +10,9 @@ public class Lexical_Analyzer {
     public Lexical_Analyzer() throws IOException {
 
        char c = ((char) br.read());
-
+      
+       char[] Operators = {'+','-' , '*' , '/' , '='};
+       
        switch (c)
        {
 
