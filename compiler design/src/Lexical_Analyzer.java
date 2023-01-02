19217596e5b7f6825d77e314fb25678605fa7409
moashqar444 <m.ashqar51@gmail.com>
2023-01-02 12:32:57 +0200

    hi

--- compiler design/src/Lexical_Analyzer.java
+++ compiler design/src/Lexical_Analyzer.java
@@ -13,12 +13,26 @@ public class Lexical_Analyzer {
       
        char[] Operators = {'+','-' , '*' , '/' , '='};
 
-       
-       switch (c)
-       {
 
-       }
+     int i;
+        BufferedReader bufferedReader;
+        while(( i=bufferedReader.read())!=-1){
+            System.out.print((char)i);
+
+            for (int j =0; j<Operators.length;j++){
+                char g =Operators[j];
+                if((char)i==g){
+
+                }
+            }
+            if (Character.isDigit((char)i)){
+
+            }
+            else{
+
+            }
 
     }
 
+}
 }
